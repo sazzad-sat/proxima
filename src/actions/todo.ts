b3d4ef558e03a75d544d5d4547380244ec1f266b
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-01 13:29:45 +0600

    add image upload on todo

--- src/actions/todo.ts
+++ src/actions/todo.ts
@@ -2,20 +2,30 @@
 
 import db from '@/db'
 import { todo } from '@/db/schema'
+import utApi from '@/server/uploadthing'
 import { eq } from 'drizzle-orm'
 import { revalidatePath } from 'next/cache'
 import { z } from 'zod'
 
+const addTodoSchema = z.object({
+  text: z.string().min(1),
+  images: z.array(z.any()).max(5),
+})
+
 export async function addTodo(path: string, formData: FormData) {
-  const todoData = z.object({
-    text: z.string().min(1),
-  })
-
-  const todoParsed = todoData.parse({
+  const todoParsed = addTodoSchema.parse({
     text: formData.get('text'),
+    images: formData.getAll('images'),
   })
 
-  await db.insert(todo).values(todoParsed)
+  const imageResponse = await utApi.uploadFiles(todoParsed.images)
+
+  const imageUrls = imageResponse.map((image) => {
+    if (image.error) throw image.error
+    return image.data?.url
+  })
+
+  await db.insert(todo).values({ text: todoParsed.text, images: imageUrls })
 
   if (path) revalidatePath(path)
 }
