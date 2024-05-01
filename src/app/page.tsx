fb15613451002aca9f84208384968e8650d7eacb
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-01 11:04:54 +0600

    sort by id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import db from '@/db'
 import { todo } from '@/db/schema'
 import Todo from './_comps/Todo'
 import AddTodo from './_comps/AddTodo'
+import { asc } from 'drizzle-orm'
+import { Accordion } from '@/components/ui/accordion'
 
 export default async function Home() {
   const todos = await db
@@ -11,15 +13,18 @@ export default async function Home() {
       status: todo.status,
     })
     .from(todo)
+    .orderBy(asc(todo.id))
 
   return (
     <main className="container space-y-4 pt-8">
-      <div className="mx-auto max-w-lg space-y-2">
+      <div className="mx-auto max-w-lg">
         <AddTodo />
 
-        {todos.map((todo) => (
-          <Todo key={todo.id} {...todo} />
-        ))}
+        <Accordion type="single" collapsible className="space-y-2">
+          {todos.map((todo) => (
+            <Todo key={todo.id} {...todo} />
+          ))}
+        </Accordion>
       </div>
     </main>
   )
