0dad8fd71d9f84e6bfc8ff2dd37d1119547d1d8d
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-03 10:23:59 +0600

    separate todolist comp

---
+++ src/app/_comps/TodoList.tsx
@@ -0,0 +1,29 @@
+import db from '@/db'
+import { todo } from '@/db/schema'
+import { asc, eq } from 'drizzle-orm'
+import { auth } from '@clerk/nextjs/server'
+import { Accordion } from '@/components/ui/accordion'
+import Todo from './Todo'
+
+export default async function TodoList() {
+  const { userId } = auth()
+
+  const todos = await db
+    .select({
+      id: todo.id,
+      text: todo.text,
+      status: todo.status,
+      images: todo.images,
+    })
+    .from(todo)
+    .where(eq(todo.creatorId, userId!))
+    .orderBy(asc(todo.id))
+
+  return (
+    <Accordion type="single" collapsible className="space-y-2">
+      {todos.map((todo) => (
+        <Todo key={todo.id} {...todo} images={todo.images as any} />
+      ))}
+    </Accordion>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
-import db from '@/db'
-import { todo } from '@/db/schema'
-import Todo from './_comps/Todo'
 import AddTodo from './_comps/AddTodo'
-import { asc, eq } from 'drizzle-orm'
-import { Accordion } from '@/components/ui/accordion'
 import Sidebar from './_comps/Sidebar'
-import { auth } from '@clerk/nextjs/server'
+import TodoList from './_comps/TodoList'
 
 export default async function Home() {
-  const { userId } = auth()
-
-  const todos = await db
-    .select({
-      id: todo.id,
-      text: todo.text,
-      status: todo.status,
-      images: todo.images,
-    })
-    .from(todo)
-    .where(eq(todo.creatorId, userId!))
-    .orderBy(asc(todo.id))
-
   return (
     <div className="min-h-screen grid grid-cols-[auto_1fr]">
       <Sidebar />
@@ -29,11 +11,7 @@ export default async function Home() {
         <div className="mx-auto max-w-lg">
           <AddTodo />
 
-          <Accordion type="single" collapsible className="space-y-2">
-            {todos.map((todo) => (
-              <Todo key={todo.id} {...todo} images={todo.images as any} />
-            ))}
-          </Accordion>
+          <TodoList />
         </div>
       </main>
     </div>
