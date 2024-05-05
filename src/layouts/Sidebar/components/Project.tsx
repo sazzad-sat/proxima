9488ef08e009e80b21bcd0e29bb5022a86bd7e73
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-05 09:39:12 +0600

    active state and onclick handler in project

--- src/layouts/Sidebar/components/Project.tsx
+++ src/layouts/Sidebar/components/Project.tsx
@@ -8,22 +8,30 @@ import { ProjectProvider } from '../contexts/project'
 type ProjectProps = {
   id: number | null
   name: string
+  active?: boolean
   className?: string
   icon?: React.ReactNode
+  onClick?: () => void
 }
 
 export default function Project(props: ProjectProps) {
-  const { id, name, className, icon } = props
+  const { id, name, className, icon, active, onClick } = props
 
   return (
     <ProjectProvider value={{ id, name }}>
-      <div className="group flex hover:bg-slate-100 rounded-lg">
+      <div
+        className={cn(
+          'group flex hover:bg-slate-100 rounded-lg',
+          active && 'bg-slate-100'
+        )}
+      >
         <Button
           className={cn(
             'gap-4 justify-start px-2 text-base font-normal w-full hover:bg-transparent',
             className
           )}
           variant={'ghost'}
+          onClick={onClick}
         >
           {icon} {name}
         </Button>
