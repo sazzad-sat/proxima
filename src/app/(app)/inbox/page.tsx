35be7ca9a28c74357a2f28dfb1421525eccdadd1
Sazzad Ahmed <sazzad.sat@gmail.com>
2024-05-06 12:13:57 +0600

    inbox header

--- src/app/(app)/inbox/page.tsx
+++ src/app/(app)/inbox/page.tsx
@@ -1,3 +1,4 @@
+import { InboxIcon } from 'lucide-react'
 import AddTodo from '../_components/AddTodo'
 import TodoList from '../_components/TodoList'
 
@@ -5,10 +6,20 @@ export default async function Inbox() {
   return (
     <main className="container space-y-4 pt-8">
       <div className="mx-auto max-w-lg">
+        <PageHeader />
+
         <AddTodo projectId={null} />
 
         <TodoList projectId={null} />
       </div>
     </main>
+  )
+}
+
+function PageHeader() {
+  return (
+    <h1 className="text-3xl font-bold my-4">
+      <InboxIcon className="inline mr-2 mb-0.5" size={28} /> Inbox
+    </h1>
   )
 }
